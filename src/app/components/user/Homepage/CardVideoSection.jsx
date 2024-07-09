c08176fd7d4b21b10193ea337a4d2dcaa16e6805
Anurag Singh <anurag.singh@fuelbuddy.in>
2024-07-09 16:38:36 +0530

    Chnages-Mine

---
+++ src/app/components/user/Homepage/CardVideoSection.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import '../Css/CardVideoSection.css'
+
+const CardVideoSection = () => {
+  return (
+    <div className="mt-5">
+      <h1 className="electrified text-5xl font-bold my-10 text-center">Global mobility ecosystem <br/> driving communities forward</h1>
+      <div>
+        
+      </div>
+   
+    </div>
+    
+  );
+};
+
+export default CardVideoSection;
